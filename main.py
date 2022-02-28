3d399264c1d888d5686916a67abc4271dbba5371
Paolo <pertino.liceograssi@gmail.com>
2022-02-28 18:11:15 +0100

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+from telegram.ext import (
+    PicklePersistence,
+    Updater,
+    CommandHandler,
+    ConversationHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    Filters,
+)
+
+import logging
+
+_DEVMODE = False
+
+
+def main():
+    print("main")
+
+
+if __name__ == "__main__":
+    main()
